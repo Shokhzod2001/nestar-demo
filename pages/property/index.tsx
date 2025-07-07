d3786824887d59406132894caff629375e9811a9
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-07-07 23:28:09 +0900

    fix: refactor pages components

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,9 +1,13 @@
-import { Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 
 const PropertyList: NextPage = () => {
-  return <Container>PROPERTY LIST</Container>;
+  return (
+    <div style={{ margin: "20px 0" }}>
+      <Stack className={"container"}>PROPERTY LIST</Stack>
+    </div>
+  );
 };
 
 export default withLayoutBasic(PropertyList);
